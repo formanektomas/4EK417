b63a2ba93b87ed9b09dd927f0bc21d3b20b7af01
Tomas Formanek <formanektomas@users.noreply.github.com>
2019-02-10 17:46:33 +0100

    Add files

---
+++ Week_01/Packages_Week_1.R
@@ -0,0 +1,22 @@
+#### Econometric seminar ####
+#
+# Packages - Week 1 
+#
+#
+install.packages(c("ggplot2","dplyr","tidyr"), dependencies = T)
+#
+install.packages("ISLR", dependencies = T)
+#
+install.packages("markdown", dependencies = T)
+#
+install.packages("yaml", dependencies = T)
+#
+install.packages("knitr", dependencies = T)
+#
+install.packages("qtl", dependencies = T)
+#
+install.packages("WDI", dependencies = T)
+#
+install.packages("countrycode", dependencies = T)
+#
+install.packages("tidyverse", dependencies = T)
