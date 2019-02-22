65af64dd4bd33efada4cd1a38cb10c4533c51088
Tomas Formanek <formanektomas@users.noreply.github.com>
2019-02-22 16:14:40 +0100

    Add files via upload

--- Week_02/W2_R_7_House_prices_ridge_lasso.R
+++ Week_02/W2_R_7_House_prices_ridge_lasso.R
@@ -1,19 +1,19 @@
 ### Modelling Prices of Census Tracts in Boston
 #
-# Apply Ridge and Lasso regression
+# Apply ridge, lasso and elastic net regression
 #
 #
 rm(list=ls())
 require(Ecdat)
 mydataset <- Hedonic
 ?Hedonic
-# mv is our dependent variable
+# mv (median value of owner–occupied homes) is our dependent variable
 x <-  model.matrix(mv~., mydataset)[1:500 , -1]
 y <- mydataset$mv[1:500]
 x.test <- model.matrix(mv~., mydataset)[501:506 , -1]
 y.test <- mydataset$mv[501:506]
 #
-# prepare a range of lambda values for ridge and lasso
+# prepare a range of lambda values for ridge, lasso and elastic net
 grid <- 10^seq(-3, 10, length=100)
 #
 #
@@ -57,6 +57,29 @@ grid <- 10^seq(-3, 10, length=100)
 
 # Step 4 - Predictions: use the "best" lasso model to predict mv values for the test sample
 
+
+
+
+#
+#
+##########################
+# Elastic net regression #
+##########################
+#
+# Step 1 - set alpha = 0.5 and use glmnet() to estimate a series of regressions for different lambdas
+
+
+
+# Step 2 - Model selection by cross-validation
+#          use CV.glmnet() to perform, store & plot the CV
+
+
+
+# Step 3 - Coefficient extraction
+
+
+
+# Step 4 - Predictions: use the "best" elastic net model to predict mv values for the test sample
 
 
 
