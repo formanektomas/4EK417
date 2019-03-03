2db41a081bcf165a9437f05b4168190959324e6e
Tomas Formanek <formanektomas@users.noreply.github.com>
2019-03-03 20:11:00 +0100

    Add files via upload

---
+++ Week_04/Packages_Week_4.R
@@ -0,0 +1,12 @@
+#### Econometric seminar ####
+#
+# Packages - Week 4
+#
+install.packages(c("ggplot2","dplyr","tidyr"), dependencies = T)
+#
+install.packages(c("sf","rgdal","rgeos","eurostat"), dependencies = T)
+#
+install.packages("RColorBrewer", dependencies = T)
+# 
+install.packages(c("spdep","splm"), dependencies = T)
+#
