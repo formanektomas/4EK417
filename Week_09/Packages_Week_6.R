21cf3c12000e349e5f832508d83dbfd873961af5
Tomas Formanek <formanektomas@users.noreply.github.com>
2019-04-07 17:01:00 +0200

    Add files via upload

---
+++ Week_09/Packages_Week_6.R
@@ -0,0 +1,23 @@
+#### Econometric seminar ####
+#
+# Packages - Week 6
+#
+install.packages("ggplot2")
+#
+install.packages("AER")
+#
+install.packages("systemfit")
+#
+install.packages("dynlm")
+#
+install.packages("orcutt")
+#
+install.packages("plm")
+#
+install.packages("car")
+#
+install.packages("lmtest")
+# 
+install.packages("progress")
+# 
+install.packages("GGally")
