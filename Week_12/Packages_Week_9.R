86ae4f60c6badc3f19f82cc50abf81e95649cc3d
Tomas Formanek <formanektomas@users.noreply.github.com>
2019-04-28 17:40:06 +0200

    Add files via upload

---
+++ Week_12/Packages_Week_9.R
@@ -0,0 +1,21 @@
+#### Econometric seminar ####
+#
+# Packages - Week 9
+#
+install.packages("ggplot2", dependencies=T)
+install.packages("MASS", dependencies=T)
+install.packages("pscl", dependencies=T)
+install.packages("AER", dependencies=T)
+install.packages("mfx", dependencies=T)
+install.packages("lmtest", dependencies=T)
+install.packages("ISLR", dependencies=T)
+install.packages("e1071", dependencies=T)
+install.packages("caret", dependencies=T)
+install.packages("ROCR", dependencies=T)
+install.packages("cvTools", dependencies=T)
+install.packages("pglm", dependencies=T)
+install.packages("lme4", dependencies=T)
+install.packages("nnet", dependencies=T)
+install.packages("Ecdat", dependencies=T)
+install.packages("ordinal", dependencies=T)
+install.packages("data.table", dependencies=T)
