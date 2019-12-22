e56c8d45a81db82a12cac88a4fb51b00fadc9fd6
Tomas Formanek <formanektomas@users.noreply.github.com>
2019-12-22 16:24:02 +0100

    Add files via upload

---
+++ Block3/R01_Packages.R
@@ -0,0 +1,10 @@
+#### Packages - Block 3 
+#
+install.packages(c("ggplot2","RColorBrewer","dplyr","reshape2","tidyr"), dependencies = T)
+#
+install.packages(c("sf","sp","rgdal","rgeos","eurostat","gstat"), dependencies = T)
+#
+install.packages(c("spdep","splm","spatialreg","raster"), dependencies = T)
+# 
+install.packages("spmoran", dependencies = T)
+#
