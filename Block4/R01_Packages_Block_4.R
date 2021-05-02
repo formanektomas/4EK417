cea2ad9767b63fee48d77090745ef2fc7f090016
Tomas Formanek <formanektomas@users.noreply.github.com>
2021-05-02 13:23:58 +0200

    Add files via upload

---
+++ Block4/R01_Packages_Block_4.R
@@ -0,0 +1,10 @@
+#### Econometric seminar ####
+#
+# Packages - Block 4
+#
+#
+install.packages(c("knitr","gridExtra","ggplot2","dplyr"), dependencies = T)
+#
+install.packages(c("lattice","foreign","Ecdat","car","lmtest"), dependencies = T)
+#
+install.packages(c("RLRsim","MEMSS","bife","sjstats","lme4","nlme","plm"), dependencies = T)
---
+++ Block4/R06_LME_wagepanel_hierarchical_data.R
@@ -0,0 +1,61 @@
+# One-dimensional panel (data with hierarchical structure)
+rm(list=ls())
+library(foreign) # install.packages("foreign")
+Panel <- read.dta("data/cpsmar2004.dta") 
+Panel$a_hga <- NULL
+Panel$a_sex <- NULL
+Panel$a_maritl <- NULL
+Panel$a_unmem <- NULL
+Panel$cellid <- NULL
+Panel <- Panel[complete.cases(Panel) == T, ] # Complete cases analysis
+#
+library(nlme)
+#
+## 1 
+#  Start by estimating an OLS model:
+summary(lm(lnwage~ed+age+asian+union, data = Panel))
+summary(lm(lnwage~ed+age+asian+union+gmstcen, data = Panel)) 
+#
+#
+## 2
+#  Using the lme() / lmer() function, allow for random effects in the intercept
+#  -> use the "gmstcen" federal state ID 
+#  .. the remaining regressors have "fixed effects" only
+#  .. interpret the results
+lme.w.1 <-
+  
+#
+#
+## 3
+#  Generalize the previous model by allowing for random effects
+#  for education (ed) variable.
+#  .. interpret the results
+lme.w.2 <-
+
+#
+#
+## 4
+#  Test for redundancy of the generalization of lme.w.1 into lme.w.2
+#  .. interpret the results
+#  ..
+# lme.w.1a <- update(lme.w.1, method="ML")
+lme.w.1a <- 
+lme.w.2a <- 
+  
+
+#
+#
+## 5
+#  Restrict the random effects from equation lme.w.2 to have cov=0.
+#  Test the "validity" of this restriction 
+lme.w.3 <- 
+
+#
+#
+## 6
+#  Include "union" as fixed effect at level 2 (state), 
+#  .. allowing for different ed slopes
+#  by amending the lme.w.2 / lme.w.3 model
+lme.w.4 <- 
+
+
